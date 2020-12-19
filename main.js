c6aa2129f05fbaa8a5b9362486aa80be9d93e660
hidao80 <8155294+hidao80@users.noreply.github.com>
2020-12-19 09:58:46 +0900

    feat: ①CWに対応　②変更のあったレーンのみ処理対象にする

--- main.js
+++ main.js
@@ -3,51 +3,49 @@ window.onload = () => {
   const itemList = document.querySelectorAll(".item-list");
   const config = {childList: true};
 
-  // 一番右のレーンのみ監視
-  itemList.forEach(line => {
+  // レーンのみ監視
+  itemList.forEach(list => {
     // 要素の監視
     const observer = new MutationObserver( (mutations,observer) => {
-      update(itemList);
+      update(list);
     });
 
-    observer.observe(line, config);
+    observer.observe(list, config);
+		update(list);	// 起動時はDOMの更新がなくても一通り処理をする
   });
-
-  update(itemList);
 };
 
-function update(itemList) {
-  itemList.forEach(line => {
-    line.querySelectorAll(".status__content").forEach(async elem => {
-      const instanceFull = "https://qiitadon.com";
-      const tootUrl = elem.textContent.match(/https?:\/\/qiitadon.com\/(@.+|web\/statuses)\/(\d+)/);
-      //console.log(tootUrl);
-      if (tootUrl !== null) {
-        const tootId = tootUrl[2];
-        console.log(instanceFull + "/api/v1/statuses/" + tootId);
-
-        const fetchArray = [fetchJsonAndCheck(`${instanceFull}/api/v1/statuses/${tootId}`)];
-        const toots = await Promise.all(fetchArray);
-        toots
-          .filter(toot => toot)
-          .forEach(toot => {
-            const tootDiv = createTootDiv(toot);
-            if (elem.querySelectorAll(".toot").length == 0) {
-              elem.appendChild(tootDiv);  
-            }
-          });
-      }
-    });
-  });
+/**
+ * 当該レーンの中で Qiitadon の toot へのリンクがあったら toot の中で展開する
+ * @param {DOM} list
+ */
+function update(list) {
+	list.querySelectorAll(".status__content").forEach(async elem => {
+		const instanceFull = "https://qiitadon.com";
+		const tootUrl = elem.textContent.match(/https?:\/\/qiitadon.com\/(@.+|web\/statuses)\/(\d+)/);
+		//console.log(tootUrl);
+		if (tootUrl !== null) {
+			const fetchArray = [fetchJsonAndCheck(`${instanceFull}/api/v1/statuses/${tootUrl[2]}`)];
+			const toots = await Promise.all(fetchArray);
+			toots.forEach(toot => {
+				console.log(toot);
+				// 展開済みでない場合のみ URL 先の toot を展開して追加する
+				if (elem.querySelectorAll(".toot").length == 0) {
+					elem.appendChild(createTootDiv(toot));
+				}
+			});
+		}
+	});
 }
 
 /**
- * @param {Request | string} input
- * @returns {Promise<any>}
+ * URL で指定されたリソースを非同期で取得する
+ * @param {string} url
+ * @returns {json | null}
  */
-async function fetchJsonAndCheck(input) {
+async function fetchJsonAndCheck(url) {
 	try {
-		const r = await fetch(input);
+		const r = await fetch(url);
 		if (r.ok) {
 			return await r.json();
 		}
@@ -58,6 +56,10 @@ async function fetchJsonAndCheck(input) {
 	}
 }
 
+/**
+ * toot を表す json から DOM を作成する
+ * @param {*} toot 
+ */
 function createTootDiv(toot) {
 	const tootDiv = document.createElement("div");
 	let strContent = toot.content;
@@ -67,30 +69,26 @@ function createTootDiv(toot) {
 			`<img class="emoji" alt=":${emoji.shortcode}:" src="${emoji.url}">`
 		);
 	}
-	const contentHtml = getHtmlFromContent(strContent);
+	const spoiler_text = toot.spoiler_text != undefined ? `<p>${toot.spoiler_text}</p>` : "";
+	const blur = toot.sensitive ? "sensitive" : "";
+	const contentHtml = document.createElement("div").innerHTML = `<div class='${blur}'>${strContent.trim()}</div>`;
 	const media = toot.media_attachments
-		.map(attachment => `<a href='${attachment.url}'><img class='thumbs' src='${attachment.preview_url}'></a>`)
+		.map(attachment => `<p><a href='${attachment.url}' target='_blank' rel='noopener noreferrer'><img class='thumbs ${blur}' src='${attachment.preview_url}'></a></p>`)
 		.join("");
 	tootDiv.innerHTML = `
 <div class="box">
 	<a href="${toot.account.url}">
-		<img width="48" height="48" alt="avatar" class="u-photo" src="${toot.account.avatar}">
+		<img width="48" height="48" alt="avatar" class="inner-u-photo" src="${toot.account.avatar}">
 	</a>
-	<a class="display-name" href="${toot.account.url}">
+	<a class="inner-display-name" href="${toot.account.url}">
 		${toot.account.display_name.trim()}
-		<span>@${toot.account.username}@${new URL(toot.account.url).hostname}</span>
+		<!--<span>@${toot.account.username}@${new URL(toot.account.url).hostname}</span>-->
 	</a>
-	<div class="e-content" lang="ja" style="display: block; direction: lt; white-space: unset;">
-		<p>${contentHtml}</p>
+	<div class="inner-e-content" lang="ja">
+		${spoiler_text}${contentHtml}
 	</div>
 	${media}
 </div>`;
-tootDiv.setAttribute("class", "toot");
+	tootDiv.setAttribute("class", "toot");
 	return tootDiv;
-}
-
-function getHtmlFromContent(strContent) {
-	const div = document.createElement("div");
-  div.innerHTML = strContent.trim();
-	return div.innerHTML;
 }
